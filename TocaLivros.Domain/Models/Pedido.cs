f4cf1a5f1f128223b0a40710d5e70ace756aa719
Allyson José <allyson.jose@outlook.com>
2016-11-30 01:42:39 -0200

    Implementação da classe Pedido

--- TocaLivros.Domain/Models/Pedido.cs
+++ TocaLivros.Domain/Models/Pedido.cs
@@ -1,7 +1,16 @@
-﻿namespace TocaLivros.Domain.Models
+﻿using System.Collections.Generic;
+
+namespace TocaLivros.Domain.Models
 {
     public class Pedido
     {
+        public Pedido()
+        {
+            Produtos = new HashSet<Produto>();
+        }
 
+        public int PedidoId { get; set; }
+        public double Valor { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; }
     }
 }
