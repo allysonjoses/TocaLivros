32bd829eca051cd1882cda89b0ed145a1ec6b463
Allyson José <allyson.jose@outlook.com>
2016-11-30 01:18:03 -0200

    Implementação da classe Usuario

--- TocaLivros.Domain/Models/Usuario.cs
+++ TocaLivros.Domain/Models/Usuario.cs
@@ -1,7 +1,16 @@
-﻿namespace TocaLivros.Domain.Models
+﻿using System.Collections.Generic;
+
+namespace TocaLivros.Domain.Models
 {
     public class Usuario
     {
+        public Usuario()
+        {
+            Pedidos = new HashSet<Pedido>();
+        }
 
+        public string UsuarioId { get; set; }
+        public string UserName { get; set; }
+        public virtual ICollection<Pedido> Pedidos { get; set; }
     }
 }
