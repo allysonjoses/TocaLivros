0d7257702e05bb33fe2e814be6903023d7c39318
Allyson José <allyson.jose@outlook.com>
2016-12-01 02:50:39 -0200

    Implementação da configuração do banco de dados

---
+++ TocaLivros.Infraestructure/EntityFramework/Context/AppDataContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Entity.ModelConfiguration.Conventions;
+using TocaLivros.Domain.Models;
+using System.Data.Entity;
+
+namespace TocaLivros.Infraestructure.EntityFramework.Context
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext() : base("name = ConnectionString")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+
+           // Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppDataContext, Configuration>());
+        }
+
+        public DbSet<Pedido> Pedido { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            #region Conventions
+
+            modelBuilder.Conventions.Remove(new ManyToManyCascadeDeleteConvention());
+            modelBuilder.Conventions.Remove(new OneToManyCascadeDeleteConvention());
+            modelBuilder.Conventions.Remove(new PluralizingTableNameConvention());
+
+            #endregion
+
+            modelBuilder.Configurations.Add(new PedidoMap());
+            modelBuilder.Configurations.Add(new ProdutoMap());
+            modelBuilder.Configurations.Add(new UsuarioMap());
+        }
+    }
+}
