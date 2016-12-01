19a5cac22edb3f1cb55bf762f03912064773feac
Allyson José <allyson.jose@outlook.com>
2016-12-01 03:34:24 -0200

    Implementação da configuração da injeção de depedencia

---
+++ TocaLivros.Infraestructure/IoC/DependencyResolver.cs
@@ -0,0 +1,18 @@
+﻿using TocaLivros.Infraestructure.EntityFramework.Repository;
+using TocaLivros.Infraestructure.EntityFramework.Context;
+using TocaLivros.Domain.Contracts;
+using Microsoft.Practices.Unity;
+
+namespace TocaLivros.Infraestructure.IoC
+{
+    public class DependencyResolver
+    {
+        public static void Resolve(UnityContainer container)
+        {
+            container.RegisterType<AppDataContext, AppDataContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<IPedidoRepository, PedidoRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<IProdutoRepository, ProdutoRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUsuarioRepository, UsuarioRepository>(new HierarchicalLifetimeManager());
+        }
+    }
+}
