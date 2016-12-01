9f2eeb9a1aa208d09f155d2da4b6b95e73399cac
Allyson José <allyson.jose@outlook.com>
2016-12-01 02:14:45 -0200

    Implementação da interface IUsuarioRepository

--- TocaLivros.Domain/Contracts/IUsuarioRepository.cs
+++ TocaLivros.Domain/Contracts/IUsuarioRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace TocaLivros.Domain.Contracts
+﻿using TocaLivros.Domain.Models;
+using System.Threading.Tasks;
+using System;
+
+namespace TocaLivros.Domain.Contracts
 {
-    public interface IUsuarioRepository
+    public interface IUsuarioRepository : IDisposable
     {
+        /// <summary>
+        /// Realiza a busca por um usuario
+        /// </summary>
+        /// <param name="username">Nome do Usuario</param>
+        /// <returns>Objeto Usuario</returns>
+        Task<Usuario> GetAsync(string username);
+
+        /// <summary>
+        /// Cria um usuario
+        /// </summary>
+        /// <param name="username">Nome do Usuario</param>
+        Task CreateAsync(string username);
     }
 }
