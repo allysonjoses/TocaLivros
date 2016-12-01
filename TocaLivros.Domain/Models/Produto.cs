393980bdfc5fd3880853ed1b4591c2610707b805
Allyson José <allyson.jose@outlook.com>
2016-12-01 04:12:06 -0200

    Correção na classe Produto

--- TocaLivros.Domain/Models/Produto.cs
+++ TocaLivros.Domain/Models/Produto.cs
@@ -1,9 +1,19 @@
-﻿namespace TocaLivros.Domain.Models
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TocaLivros.Domain.Models
 {
     public class Produto
     {
+        public Produto()
+        {
+            Pedidos = new HashSet<Pedido>();
+        }
+
         public int ProdutoId { get; set; }
         public string Nome { get; set; }
         public double Valor { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Pedido> Pedidos { get; set; }
     }
 }
