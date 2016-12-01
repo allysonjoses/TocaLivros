db40629d3ce3696b2906f8a48d3e941b4212942d
Allyson José <allyson.jose@outlook.com>
2016-12-01 03:25:24 -0200

    Implementação do mapeamento da classe Usuario e Produto

---
+++ TocaLivros.Infraestructure/EntityFramework/Map/ProdutoMap.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using TocaLivros.Domain.Models;
+
+namespace TocaLivros.Infraestructure.EntityFramework.Map
+{
+    public class ProdutoMap : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoMap()
+        {
+            ToTable("Produto");
+
+            Property(x => x.ProdutoId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(x => x.Nome).HasMaxLength(50).IsRequired();
+
+            HasMany<Pedido>(x => x.Pedidos).WithMany(x => x.Produtos).Map(x =>
+            {
+                x.MapLeftKey("ProdutoId");
+                x.MapRightKey("PedidoId");
+                x.ToTable("PedidoItens");
+            });
+        }
+    }
+}
---
+++ TocaLivros.Infraestructure/EntityFramework/Map/UsuarioMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity.Infrastructure.Annotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using TocaLivros.Domain.Models;
+
+namespace TocaLivros.Infraestructure.EntityFramework.Map
+{
+    public class UsuarioMap : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioMap()
+        {
+            ToTable("Usuario");
+
+            Property(x => x.UsuarioId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.UserName).HasMaxLength(50).
+                HasColumnAnnotation("Index", new IndexAnnotation
+                (new IndexAttribute("IX_Username") { IsUnique = true })).IsRequired();
+        }
+    }
+}
