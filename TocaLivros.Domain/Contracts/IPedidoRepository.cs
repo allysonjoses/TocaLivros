7176e9929022208f5b70d794fc624974a48966f7
Allyson José <allyson.jose@outlook.com>
2016-12-01 02:41:04 -0200

    Implementação da interface IPedidoRepository

--- TocaLivros.Domain/Contracts/IPedidoRepository.cs
+++ TocaLivros.Domain/Contracts/IPedidoRepository.cs
@@ -1,6 +1,28 @@
-﻿namespace TocaLivros.Domain.Contracts
+﻿using TocaLivros.Domain.Models;
+using System.Threading.Tasks;
+using System;
+
+namespace TocaLivros.Domain.Contracts
 {
-    public interface IPedidoRepository
+    public interface IPedidoRepository : IDisposable
     {
+        /// <summary>
+        /// Realiza a busca por um pedido
+        /// </summary>
+        /// <param name="id">Id do pedido</param>
+        /// <returns>Objeto Pedido</returns>
+        Task<Pedido> GetAsync(int id);
+
+        /// <summary>
+        /// Cria um pedido
+        /// </summary>
+        /// <param name="pedido">Pedido a ser criado</param>
+        Task CreateAsync(Pedido pedido);
+
+        /// <summary>
+        /// Atualiza um produto
+        /// </summary>
+        /// <param name="pedido">pedido com as alterações realizadas</param>
+        Task UpdateAsync(Pedido pedido);
     }
 }
