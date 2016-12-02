9c0bae0ebbfe63e16952ab4100bfd07caef09e53
Allyson José <allyson.jose@outlook.com>
2016-12-02 05:28:33 -0200

    Implementação de PedidoController

--- TocaLivros.API/Controllers/PedidoController.cs
+++ TocaLivros.API/Controllers/PedidoController.cs
@@ -1,8 +1,83 @@
-﻿using System.Web.Http;
+﻿using TocaLivros.Domain.Contracts;
+using System.Web.Http;
+using System.Threading.Tasks;
+using System.Net.Http;
+using TocaLivros.Domain.Models;
+using System.Net;
+using System;
 
 namespace TocaLivros.API.Controllers
 {
+    [RoutePrefix("api/pedido")]
     public class PedidoController : ApiController
     {
+        private readonly IPedidoRepository _DataBase;
+
+        public PedidoController(IPedidoRepository DataBase)
+        {
+            this._DataBase = DataBase;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("{pedidoId}")]
+        public async Task<HttpResponseMessage> GetAsync(int pedidoId)
+        {
+            var response = new HttpResponseMessage();
+
+            try
+            {
+                response = Request.CreateResponse(HttpStatusCode.OK, await _DataBase.GetAsync(pedidoId));
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+            return response;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<HttpResponseMessage> CreateAsync(Pedido pedido)
+        {
+            var response = new HttpResponseMessage();
+
+            try
+            {
+                await _DataBase.CreateAsync(pedido);
+                response = Request.CreateResponse(HttpStatusCode.Created);
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+            return response;
+        }
+
+        [HttpPut]
+        [Route("")]
+        public async Task<HttpResponseMessage> UpdateAsync(Pedido pedido)
+        {
+            var response = new HttpResponseMessage();
+
+            try
+            {
+                await _DataBase.UpdateAsync(pedido);
+                response = Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+            return response;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _DataBase.Dispose();
+        }
     }
 }
