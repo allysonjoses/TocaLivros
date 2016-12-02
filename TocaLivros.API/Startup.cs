b78739cf528f6ee0446ee0b959c5bd94ef24ba2c
Allyson José <allyson.jose@outlook.com>
2016-12-02 03:18:27 -0200

    Implementação geral da API

---
+++ TocaLivros.API/Startup.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json.Serialization;
+using Microsoft.Owin.Security.OAuth;
+using TocaLivros.Domain.Contracts;
+using Microsoft.Practices.Unity;
+using TocaLivros.API.Security;
+using TocaLivros.API.Helpers;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Microsoft.Owin;
+using TocaLivros.API;
+using System;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace TocaLivros.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Configure Dependency Injection
+            var container = new UnityContainer();
+            Infraestructure.IoC.DependencyResolver.Resolve(container);
+            config.DependencyResolver = new UnityResolver(container);
+
+            ConfigureWebApi(config);
+            ConfigureOAuth(app, container.Resolve<IUsuarioRepository>());
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            // Remove o XML
+            var formatters = config.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+
+            // Modifica a identação
+            var jsonSettings = formatters.JsonFormatter.SerializerSettings;
+            jsonSettings.Formatting = Formatting.Indented;
+            jsonSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // Modifica a serialização
+            formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        public void ConfigureOAuth(IAppBuilder app, IUsuarioRepository service)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/api/authentication"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(24),
+                Provider = new AuthorizationServerProvider(service)
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
